2b7f6336a802b847448e67bbc6ffc13a311fde11
Daniel Rivera <daniel@macadamian.com>
2012-07-17 22:16:25 -0400

    Split out css and js from panel sample index.html to own files.

---
+++ Transforms, Transitions and Animations/panel/js/app.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2012 Macadamian Technologies Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+ $(document).ready(function() {
+    // transitions?
+    if (!Modernizr.csstransitions) {
+        $("[role=main]").html("This browser does not support CSS transitions which are required for this demo.");
+    }
+    else {
+        // bind functionality to buttons                
+        $("#btnToggle").click(function() {
+            $("#divPanel").toggleClass("collapsed");
+            $("#divPanel").toggleClass("uncollapsed");
+        });
+    }
+});
